f8665b4cf4a1d3b33f6a22ac05239d0e4334456c
Justin-K-Ellis <jlke45@gmail.com>
2022-12-29 23:03:52 +0900

    Updated REASME.md and added Descartes to philo_madlibs.py

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 This is a simple madlibs-style program where the user can choose a famous philosopher and modify the opening of one of their famous works (or, in the case of Wittgenstien, the top-level propositions of the _Tractatus_). Currently, the following philosophers and text are available:
 1. Plato (_Republic_)
 2. Aristotle (_Metaphysics_)
-3. Rene Descartes (_Meditations of First Philosophy_ (text coming soon))
+3. Rene Descartes (_Meditations on First Philosophy_)
 4. David Hume (_An Enquiry concerning Human Understanding_)
 5. Ludwig Wittgenstein (_Tractatus Logico-Philosophicus_)
 
--- philo_madlibs.py
+++ philo_madlibs.py
@@ -18,7 +18,9 @@ def republic():
     When we had {past_verb2} our prayers and viewed the spectacle, we turned in the direction of the city; 
     and at that instant Polemarchus the {noun3} of Cephalus chanced to catch sight of us from a distance as 
     we were {cont_verb}, and told his servant to run and bid us wait for him. The servant 
-    took hold of me by the {cloth_or_body}, and said: Polemarchus desires you to {verb2}.\nPlato, Republic\n"""
+    took hold of me by the {cloth_or_body}, and said: Polemarchus desires you to {verb2}.
+    
+    Plato, Republic\n"""
 
     print(republic_intro)
 
@@ -27,23 +29,45 @@ def metaphysics():
     verb1 = input("Give me a verb: ")
     noun2 = input("Give me another plural noun: ")
     noun3 = input("And another plural noun, please: ")
-    past_verb1 = input("Now give me a past-tense verb: ")
+    past_verb1 = input("Now give me a past particible verb: ")
     body_part = input("Now give me a body part: ")
     verb3 = input("Now give me another present-tense verb: ")
     noun4 = input("Last plural noun, I swear: ")
     verb4 = input("Last, one more present-tense verb: ")
 
     metaphysics_intro = f"""
-    All {noun1} by nature desired to {verb1}. An indication of this is our liking for {noun2}.
+    All {noun1} by nature desires to {verb1}. An indication of this is our liking for {noun2}.
     For even apart from their {noun3}, these are {past_verb1} because of themselves--and most of all because of the 
     {body_part}. For it is not only in order to {verb3} an action, but even when we are not going to do anything
     whatsoever, that we choose {noun4} over (one might almost say) all the others. The cause of this is that of all
-    {noun1} it enables us to {verb4} most fully and makes clear many difficulties.\nAristotle, Metaphysics\n"""
+    {noun1} it enables us to {verb4} most fully and makes clear many difficulties.
+
+    Aristotle, Metaphysics\n"""
 
     print(metaphysics_intro)
 
 def mediations():
-    pass
+    false = input("Give me an adjective: ")
+    beliefs = input("Give me a plural noun: ")
+    true = input("Give me an adjective: ")
+    constructed = input("Give me a past-tense verb: ")
+    seriously = input("Give me an adverb: ")
+    opinions = input("Give me another plural noun: ")
+    establish = input("Give me a present-tense verb: ")
+    sciences = input("Last, give me one more plural noun: ")
+
+    medi_intro = f"""
+    It is now some years since I detected how many were the {false}
+    {beliefs} that I had from my earliest youth admitted as {true}, and how
+    doubtful was everything I had since {constructed} on this basis; and from
+    that time I was convinced that I must once for all {seriously} undertake to
+    rid myself of all the {opinions} which I had formerly accepted, and
+    commence to build anew from the foundation, if I wanted to {establish}
+    any firm and permanent structure in the {sciences}.
+
+    Rene Descartes, Meditations\n"""
+
+    print(medi_intro)
 
 def enquiry():
     noun1 = input("Give me a noun: ")
@@ -64,7 +88,9 @@ def enquiry():
     to the light in which they {verb2} themselves. As virtue, of all objects, is allowed to be the most {adj2}, this 
     species of philosophers paint her in the most amiable colours; {ing_verb1} all helps from poetry and eloquence, and 
     treating their subject in an easy and obvious manner, and such as is best fitted to please the imagination, and 
-    engage the {noun3}.\n"""
+    engage the {noun3}.
+    
+    David Hume, An Enquiry concerning Human Understanding\n"""
 
     print(enquiry_intro)
 
@@ -86,7 +112,9 @@ def tractatus():
     4. A {thought} is a {proposition} with a sense.
     5. A {proposition} is a {truth_function} of elementary {proposition}s. (An elementary {proposition} is a {truth_function} of itself.)
     6. The general form of a {proposition} is the general form of a {truth_function}, which is: [{symbol}]. This is the general form of a {proposition}.
-    7. Whereof one cannot {speak}, thereof one must {be_silent}."""
+    7. Whereof one cannot {speak}, thereof one must {be_silent}.
+    
+    Ludwig Wittgenstein, Tractatus Logico-Philosophicus\n"""
 
     print(tract_props)
 
@@ -109,6 +137,6 @@ def main():
         elif choice == "5":
             tractatus()
         else:
-            print("Try again.")
+            print("Try again.\n")
 
 main()
